495c9cfd43e785b0543269742ec9fd4e2bbe6be9
Bart Sokol <bartsokol@users.noreply.github.com>
2018-07-03 12:43:03 +0200

    Generate the documentation based on md/fsx and API docs

---
+++ docs/content/index.fsx
@@ -0,0 +1,47 @@
+(*** hide ***)
+#r "../../src/UkModulusCheck/bin/Release/net461/UkModulusCheck.dll"
+
+(**
+# UK Modulus Check
+
+This library provides the functions to validate UK sort code and account number
+according to the specification provided by [VocaLink](https://www.vocalink.com/customer-support/modulus-checking/).
+
+The library is implemented in F# and can be use from any .NET project.
+
+## Usage
+
+To check the sort code / account number pair you'll need:
+
+* [Rules and weightings table](https://www.vocalink.com/media/3003/valacdos-v490.txt)
+* [Sort code substitution table](https://www.vocalink.com/media/1517/scsubtab.txt).
+
+You can obtain newest version of both files from [VocaLink](https://www.vocalink.com/customer-support/modulus-checking/).
+
+Once you got the files, you can load the data into memory (and potentially cache it) using provided functions:
+*)
+
+open UkModulusCheck.Validator
+
+let rules =
+    match loadRules "valacdos-v490.txt" with
+    | Ok r -> r
+    | Error ex -> [] // Handle loading error
+let substitutions =
+    match loadSubstitutions "scsubtab.txt" with
+    | Ok s -> s
+    | Error ex -> [] // Handle loading error
+
+(**
+You can find the details of the functions in [the reference docs](reference/ukmoduluscheck-validator.html).
+
+To validate the sort code and account number pair use `validateAccountNo` function:
+*)
+
+match validateAccountNo rules substitutions sortCode accountNumber with
+| Valid -> printfn "%s-%s is valid!" sortCode accountNumber
+| Invalid -> printfn "%s-%s is invalid!" sortCode accountNumber
+
+(**
+For more information about the API and types used by the library please check [the reference docs](reference/index.html).
+*)
--- src/UkModulusCheck/Types.fs
+++ src/UkModulusCheck/Types.fs
@@ -1,16 +1,25 @@
 namespace UkModulusCheck
 
+/// Types used by the API of the library
 module Types =
+
+    /// The result of the validation
     type ValidationResult = Valid | Invalid
 
+    /// The available validation algorithms
     type ValidationMethod = Standard10 | Standard11 | DoubleAlternate
 
+    /// The exeception from the standard validation rules as described in the VocaLink documentation
     type ValidationException = Exception of int
 
+    /// Alias type for sort code
     type SortCode = string
+    /// Alias type for account number
     type AccountNumber = string
+    /// Alias type for position weight
     type Weight = int
 
+    /// Captures the parsed line from from the validation rules table provided by VocaLink
     type ValidationRule =
         {
             SortCodeFrom: SortCode
@@ -20,4 +29,5 @@ module Types =
             Exception: ValidationException option
         }
 
+    /// Captures the parsed line from from the sort code substitutions table provided by VocaLink
     type SortCodeSubstitution = { SortCode: SortCode; SubstituteWith: SortCode }
